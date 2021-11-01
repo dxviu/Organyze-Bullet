5839be3b7618488f09e4bdb310f2672db5a1e09f
dxviu <79173669+dxviu@users.noreply.github.com>
2021-11-01 17:50:44 -0400

    Update text-field-input.dart

--- lib/widgets/text-field-input.dart
+++ lib/widgets/text-field-input.dart
@@ -19,11 +19,8 @@ class TextInputField extends StatelessWidget {
         height: size.height * 0.08,
         width: size.width * 0.8,
         decoration: BoxDecoration(
-          Opacity(
-          color: Colors.grey[500],
-          opacity: 0.5;
+          color: Colors.grey[500]!.withOpacity(0.5),
           borderRadius: BorderRadius.circular(16),
-            ),
         ),
         child: Center(
           child: TextField(
