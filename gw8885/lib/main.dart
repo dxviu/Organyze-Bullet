4a8437f806d47282730c758a3d26e245fc34938f
Ibrahim-gw8885 <gw8885@wayne.edu>
2021-11-02 14:41:00 -0400

    inputFields go to the database, create account only

---
+++ gw8885/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
+import 'package:organyzebullet_app/screens/home-screen.dart';
+import 'screens/screens.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:organyzebullet_app/database/realtime_database_function.dart';
+import 'package:organyzebullet_app/database/dataModel.dart';
+import 'package:organyzebullet_app/database/message_dao.dart';
+void main() async {
+  //await Firebase.initializeApp();
+  //dbfunc oz = new dbfunc();
+  //oz.createUser(2, 'username', 'email@email.com', 'password1');
+  print("message");
+  WidgetsFlutterBinding.ensureInitialized(); //this is IMPORTANT to not have a null error message
+  runApp(MyApp());
+
+}
+
+/*class dbfunc extends realtime {
+  @override
+  Future<void> createUser(int ID, String username, String email, String password) async {
+    final account = database.child('UID/');
+    return Future.delayed(
+        const Duration(milliseconds:1), () => account.set({'UID': ID,'Username':username,'Email':email,'Password':password}));
+  }
+}*/
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+   //final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Organyze Bullet',
+      theme: ThemeData(
+        //textTheme:GoogleFonts.josefinSansTextTheme(Theme.of(context).textTheme),
+        primarySwatch: Colors.brown,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      /*home: FutureBuilder(
+        future: _fbApp,
+            builder: (context,snapshot) {
+              if(snapshot.hasError) {
+                print('You have an error! ${snapshot.error.toString()}');
+                return Text('Something went wrong');}
+                else if (snapshot.hasData) {
+                  return LoginScreen();}
+                else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                  }
+                },
+      ),*/
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        'ForgotPassword': (context) => ForgotPassword(),
+        'CreateNewAccount': (context) => CreateNewAccount(),
+        //'HomeScreen': (context) => HomeScreen(),
+      },
+    );
+
+
+
+  }
+
+
+
+
+}
+
+
+
+
---
+++ gw8885/lib/screens/create-new-account.dart
@@ -0,0 +1,219 @@
+import 'dart:ui';
+import 'package:organyzebullet_app/database/dataModel.dart';
+import 'package:organyzebullet_app/database/message_dao.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+//import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/firebase_database.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:organyzebullet_app/pallete.dart';
+import 'package:organyzebullet_app/widgets/widgets.dart';
+
+
+
+class CreateNewAccount extends StatelessWidget {
+  //final Future<database> = FirebaseDatabase.instance.reference();
+  //FirebaseAuth auth = FirebaseAuth.instance;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    int idNumo = 1;
+    //final account = database.child('UID/');
+    final usero = TextEditingController();
+    final emailo = TextEditingController();
+    //final _password = TextEditingController();
+
+
+
+    usero.addListener(() => print('first text field: ${usero.text}'));
+    emailo.addListener(() => print('first text field: ${emailo.text}'));
+
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        BackgroundImage(image: 'assets/images/reg.png'),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.width * 0.1,
+                ),
+                Stack(
+                  children: [
+                    Center(
+                      child: ClipOval(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                          child: CircleAvatar(
+                            radius: size.width * 0.14,
+                            backgroundColor: Colors.grey[400].withOpacity(
+                              0.4,
+                            ),
+                            child: Icon(
+                              FontAwesomeIcons.user,
+                              color: kWhite,
+                              size: size.width * 0.1,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: size.height * 0.08,
+                      left: size.width * 0.56,
+                      child: Container(
+                        height: size.width * 0.1,
+                        width: size.width * 0.1,
+                        decoration: BoxDecoration(
+                          color: kBlue,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: kWhite, width: 2),
+                        ),
+                        child: Icon(
+                          FontAwesomeIcons.arrowUp,
+                          color: kWhite,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: size.width * 0.1,
+                ),
+                Column(
+                  children: [
+                    TextInputField(
+                      icon: FontAwesomeIcons.user,
+                      hint: 'User',
+                      inputType: TextInputType.name,
+                      inputAction: TextInputAction.next,
+                      myController: usero,
+
+                    ),
+                    TextInputField(
+                      icon: FontAwesomeIcons.envelope,
+                      hint: 'Email',
+                      inputType: TextInputType.emailAddress,
+                      inputAction: TextInputAction.next,
+                      myController: emailo,
+                    ),
+                    PasswordInput(
+                      icon: FontAwesomeIcons.lock,
+                      hint: 'Password',
+                      inputAction: TextInputAction.next,
+                    ),
+                    PasswordInput(
+                      icon: FontAwesomeIcons.lock,
+                      hint: 'Confirm Password',
+                      inputAction: TextInputAction.done,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {
+                        idNumo = idNumo + 1,
+                      _sendMessage(usero.text, idNumo, emailo.text, "password")
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Text(
+                          "Create Account"
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account?',
+                          style: kBodyText,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/');
+                          },
+                          child: Text(
+                            'Login',
+                            style: kBodyText.copyWith(
+                                color: kBlue, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+       // FutureBuilder()
+      ],
+
+    );
+
+
+
+  }
+
+
+
+  void _sendMessage(String nameWrite,int idNum, String emailWrite, String passwordWrite) {
+    if (_canSendMessage()) {
+      final message = oUser(nameWrite, idNum ,emailWrite,passwordWrite);
+      final messageDao = MessageDao();
+      print("hi");
+      messageDao.saveMessage(message);
+    }
+  }
+
+  /*Widget _getMessageList() {
+    return Expanded(
+      child: FirebaseAnimatedList(
+        query: widget.messageDao.getMessageQuery(),
+        itemBuilder: (context, snapshot, animation, index) {
+          final json = snapshot.value as Map<dynamic, dynamic>;
+          final message = oUser.fromJson(json);
+          return MessageWidget(message.text, message.date);
+        },
+      ),
+    );
+  }
+
+
+
+  void _scrollToBottom() {
+    if (_scrollController.hasClients) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    }
+  }*/
+  bool _canSendMessage() => true;
+}
+
+class MessageList extends StatefulWidget {
+  MessageList({Key key}) : super(key: key);
+
+  final messageDao = MessageDao();
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+  //MessageListState createState() => MessageListState();
+}
