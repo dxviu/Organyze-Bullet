002948305dc51fa5447774c119f64fe8a4b73db5
Jordan Y <79173669+dxviu@users.noreply.github.com>
2021-11-09 17:54:25 -0500

    Update share-notebook.dart

--- lib/screens/share-notebook.dart
+++ lib/screens/share-notebook.dart
@@ -1,1 +1,27 @@
+import 'dart:ui';
+import 'package:organyzebullet_app/database/dataModel.dart';
+import 'package:organyzebullet_app/database/message_dao.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:organyzebullet_app/pallete.dart';
+import 'package:organyzebullet_app/widgets/widgets.dart';
 
+  
+public class Client
+{
+    public static void main (String[] args)
+    {
+        // Internet internet = new ProxyInternet();
+        try
+        {
+            
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
