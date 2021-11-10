1b8ae29c75d63a823e9176f4b4fd0d8d9231d032
Jordan Y <79173669+dxviu@users.noreply.github.com>
2021-11-09 19:04:48 -0500

    Update share-notebook.dart

--- lib/screens/share-notebook.dart
+++ lib/screens/share-notebook.dart
@@ -9,19 +9,77 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:organyzebullet_app/pallete.dart';
 import 'package:organyzebullet_app/widgets/widgets.dart';
 
-  
-public class Client
+// Example for Adapter Design no code from firebase
+interface User
 {
-    public static void main (String[] args)
+    public void signedUser();
+    public void randomUser();
+}
+  
+class NotebookURL implements User
+{
+    // a concrete implementation of bird
+    public void signedUser()
     {
-        // Internet internet = new ProxyInternet();
-        try
-        {
-            
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
+        System.out.println("Accessing");
+    }
+    public void randomUser()
+    {
+        System.out.println("Access Denied");
+    }
+}
+
+interface GoogleSignIn
+{
+    public void signed();
+}
+  
+class Google implements GoogleSignIn
+{
+    public void signed()
+    {
+        System.out.println("Make an account?");
+    }
+}
+  
+class SignInAdapter implements GoogleSignIn
+{
+    // You need to implement the interface your
+    // client expects to use.
+    User user;
+    public SignInAdapter(User user)
+    {
+        // we need reference to the object we
+        // are adapting
+        this.user = user;
+    }
+  
+    public void squeak()
+    {
+        // translate the methods appropriately
+        user.makeSound();
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        NotebookURL notebook = new NotebookURL();
+  
+        GoogleSignIn signinAdapter = new SignInAdapter(user);
+  
+        System.out.println("User enter");
+      // correct
+        user.signedUser();
+      // incorrect
+        user.randomUser();
+  
+        System.out.println("Google");
+      // ask to make an account
+        google.signed();
+  
+        System.out.println("Adapter");
+      // decline to make an account -> looks at Notebook URL
+        signInAdapter.signedUser();
     }
 }
