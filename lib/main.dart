556ba7b40540c58f41a5c38fb4a8c9d61694a601
Jordan Y <79173669+dxviu@users.noreply.github.com>
2021-11-18 20:34:04 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:organyzebullet_app/database/realtime_database_function.dart';
 import 'package:organyzebullet_app/database/dataModel.dart';
 import 'package:organyzebullet_app/database/message_dao.dart';
-import 'dart:async';
+import 'package:async/async.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); //this is IMPORTANT to not have a null error message
