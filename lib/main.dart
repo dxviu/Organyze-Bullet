3aaa72866367052666018b90b6c93890dfabfb26
Jordan Y <79173669+dxviu@users.noreply.github.com>
2021-12-07 18:52:23 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
         'viewNotebooks':(context) => viewNotebooks(),
         'AddNewNotebook':(context) => AddNewNotebook(),
         'settings-screen':(context) => Settings(),
-        'updateEntry':(context) => updateEntry()
+        'updateEntry':(context) => updateEntry(),
         'view_search':(context) => viewSearch(),
         'add-search':(context) => addSearch(),
       },
